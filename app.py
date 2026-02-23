7f0ece41659cbcaa747eb361d30ca7049967a9d6
mukesh-kanth <varunharish0786@gmail.com>
2026-02-23 14:20:09 +0530

    Production configuration update

--- app.py
+++ app.py
@@ -40,5 +40,11 @@ def detect():
 
     return render_template("detect.html", title="Detection Page")
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
